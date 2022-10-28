59ba2ccf73ac80cbff35e8483bdba13620b7129d
Mateus-OSilva <mateusods20@gmail.com>
2022-10-27 22:45:56 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação esta se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("Arquivo feito sem consentimenot hihi")
+console.log("enviando para o github")
